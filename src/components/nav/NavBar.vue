8f58b9e9d1cc09a99cb804b9083bb4f754f264eb
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-14 15:16:40 +0100

    work

---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <p>Logo</p>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/nav/nav-bar';
+</style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBar from '@/components/nav/NavBar.vue';
+</script>
+
+<template>
+  <NavBar />
+
+  <router-view />
+</template>
+
+<style lang="scss" scoped></style>
