4b3f35d9a79fb2818a26f4e945fa8bb851437b74
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-14 15:15:10 +0100

    added dynamic layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import { shallowRef, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import BaseLayout from '@/layouts/BaseLayout.vue';
+
+const layout = shallowRef(null);
+const route = useRoute();
+
+watch(route, () => {
+  route.meta.layout !== undefined
+    ? (layout.value = route.meta.layout)
+    : (layout.value = BaseLayout); // this is default layout if route meta is not set;
+});
+</script>
 
 <template>
-  <router-view />
+  <component :is="layout" />
 </template>
 
 <style scoped></style>
