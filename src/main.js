4817729c1bc4d820099050e38ca2d5cbd201d381
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-15 13:47:19 +0100

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -3,5 +3,14 @@ import './style.css';
 import App from './App.vue';
 import router from '@/router/index';
 import '@/sass/main.scss';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-createApp(App).use(router).mount('#app');
+import { faSearch, faHeart, faFilter } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSearch, faHeart, faFilter);
+
+createApp(App)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
