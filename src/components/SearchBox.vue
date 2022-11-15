dc488f49ff68783956b4822a9464688dadb0315b
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-15 13:48:39 +0100

    work on searchbar

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="search-box">
+    <h1 class="search-box__heading">What would you like to cook?</h1>
+    <div class="input-container">
+      <p class="filter-btn">Filter</p>
+      <font-awesome-icon icon="search" class="input-icon" />
+      <input
+        class="input-field"
+        type="text"
+        placeholder="Recepies, ingredients"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/search-box';
+</style>
