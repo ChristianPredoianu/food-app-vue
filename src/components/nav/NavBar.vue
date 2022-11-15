de194825b423033c31b40f13402aa51bd596ddca
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-15 13:47:05 +0100

    navbar final

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,9 +1,46 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const isNavOpen = ref(false);
+const isMobileView = ref(true);
+
+function toggleNav() {
+  isNavOpen.value = !isNavOpen.value;
+}
+
+function handleView() {
+  isMobileView.value = window.innerWidth < 640;
+  isMobileView.value ? (isNavOpen.value = false) : (isNavOpen.value = true);
+}
+
+onMounted(() => {
+  handleView();
+  window.addEventListener('resize', handleView);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleView);
+});
+</script>
 
 <template>
-  <header>
-    <nav>
-      <p>Logo</p>
+  <header class="header">
+    <nav class="nav container">
+      <p class="logo">Foo<span class="logo__span">die</span></p>
+      <ul class="nav-links" v-if="isNavOpen">
+        <li class="nav-links__item">
+          <font-awesome-icon icon="search" />Search
+        </li>
+        <li class="nav-links__item">
+          <font-awesome-icon icon="heart" />Favorite
+        </li>
+        <li class="nav-links__item nav-links__item--btn-transparent">Login</li>
+        <li class="nav-links__item nav-links__item--btn-green">Sign up</li>
+      </ul>
+      <div class="hamburger" :class="{ active: isNavOpen }" @click="toggleNav">
+        <span class="hamburger__bar"></span>
+        <span class="hamburger__bar"></span>
+      </div>
     </nav>
   </header>
 </template>
