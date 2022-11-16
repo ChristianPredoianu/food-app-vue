49d0ae413787c9e8eb36877618280996b6e5512d
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-16 09:49:50 +0100

    added heart icon

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import '@/sass/main.scss';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import { faSearch, faHeart, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 library.add(faSearch, faHeart, faFilter);
 
