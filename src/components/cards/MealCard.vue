3da108b7ed6c35cc070d597b065af636095f378c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-16 15:07:23 +0100

    changed some vars

--- src/components/cards/MealCard.vue
+++ src/components/cards/MealCard.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  meal: Object,
+});
+</script>
 
-<template></template>
+<template>
+  <div class="meal-card">
+    <img :src="meal.recipe.image" alt="meal" class="meal-card__img" />
+    <h3 class="meal-card__heading">{{ meal.recipe.label }}</h3>
+    <div class="meal-card-cta">
+      <font-awesome-icon icon="fa-regular fa-heart" class="meal-card__icon" />
+      <button class="meal-card-cta__btn">Details</button>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '@/sass/components/cards/meal-card';
+</style>
