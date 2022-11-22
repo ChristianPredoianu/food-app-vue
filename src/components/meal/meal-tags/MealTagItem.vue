7caf7000807f0187e1f746efb0d7301c367dfab1
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-22 13:24:09 +0100

    added meall tags

---
+++ src/components/meal/meal-tags/MealTagItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  mealTags: Array,
+});
+</script>
+
+<template>
+  <li class="meal-tag-item" v-for="mealTag in mealTags"># {{ mealTag }}</li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/meal-tags/meal-tag-item';
+</style>
---
+++ src/components/meal/meal-tags/MealTagsList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MealTagItem from '@/components/meal/meal-tags/MealTagItem.vue';
+
+const props = defineProps({
+  mealTags: Array,
+});
+</script>
+
+<template>
+  <ul class="meal-tags-list">
+    <MealTagItem :mealTags="mealTags" />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/meal-tags/meal-tags-list';
+</style>
