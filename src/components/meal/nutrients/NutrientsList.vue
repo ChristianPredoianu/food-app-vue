53d2f2af2c911b3562156b2cbe896e1df396deb1
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-22 13:24:55 +0100

    added nutrients

---
+++ src/components/meal/nutrients/NutrientsList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NutrientsListItem from '@/components/meal/nutrients/NutrientsListItem.vue';
+
+const props = defineProps({
+  mealNutrients: Object,
+});
+</script>
+
+<template>
+  <ul class="nutrients-list">
+    <NutrientsListItem :mealNutrients="mealNutrients" />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/nutrients/nutrients-list';
+</style>
---
+++ src/components/meal/nutrients/NutrientsListItem.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useRoundNumber } from '@/composables/useRoundNumber';
+
+const props = defineProps({
+  mealNutrients: Object,
+});
+
+const { roundNumber } = useRoundNumber();
+</script>
+
+<template>
+  <li
+    class="nutrients-list-item"
+    v-for="mealNutrient in mealNutrients"
+    :key="mealNutrient.label"
+  >
+    <div class="meal-nutrients">
+      <div class="nutrient">
+        <p class="nutrient__label">{{ mealNutrient.label }}</p>
+        <p class="nutrient__value">{{ roundNumber(mealNutrient.quantity) }}%</p>
+      </div>
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/nutrients/nutrients-list-item';
+</style>
