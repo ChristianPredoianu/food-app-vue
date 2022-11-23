b68014ff67b31cf9225b86cf38c4196e6f20d29c
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-23 08:50:16 +0100

    changed file name

---
+++ src/components/meal/meal-tags/MealTagListItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  mealTags: Array,
+});
+</script>
+
+<template>
+  <li class="meal-tag-item" v-for="mealTag in mealTags" :key="mealTag">
+    # {{ mealTag }}
+  </li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/meal-tags/meal-tag-list-item';
+</style>
