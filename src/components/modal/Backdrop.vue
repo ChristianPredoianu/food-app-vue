4f0467ac3306fe0b022c4950573ef038acd31953
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-24 14:35:33 +0100

    added modal

---
+++ src/components/modal/Backdrop.vue
@@ -0,0 +1,19 @@
+<script setup>
+const emit = defineEmits(['closeModall']);
+
+function onCloseModal() {
+  emit('closeModall');
+}
+</script>
+
+<template>
+  <div>
+    <teleport to="#backdrop-root">
+      <div class="backdrop" @click="onCloseModal"></div>
+    </teleport>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/modal/backdrop';
+</style>
