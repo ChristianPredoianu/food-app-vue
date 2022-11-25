ba9e20b2f2b5ce18cab2269c6419ae3f0f37f601
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-25 17:22:37 +0100

    removed animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -87,18 +87,4 @@ async function fetchMoreMeals() {
 @use '@/sass/views/home';
 </style>
 
-<style lang="scss" scoped>
-.slide-fade-enter-active {
-  transition: all 10.3s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 10.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
-</style>
+<style lang="scss" scoped></style>
