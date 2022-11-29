06e78ebcc6940ac1ddfb70ef4dd6d4beb0c4158e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-29 12:25:57 +0100

    added modal and backdrop component in a div

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@ import SearchBox from '@/components/SearchBox.vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import MealCard from '@/components/cards/MealCard.vue';
 import Backdrop from '@/components/modal/Backdrop.vue';
+import Modal from '@/components/modal/Modal.vue';
 
 const state = reactive({
   mealsData: null,
@@ -27,6 +28,25 @@ const { isModalOpen, openModal, closeModal } = useModal();
 const mealsUrl =
   'https://api.edamam.com/api/recipes/v2?type=public&app_id=2d7284f7&app_key=0a6f557d15da76ad2dea06845fbe542c&diet=balanced&dishType=Main%20course';
 
+onBeforeMount(async () => {
+  const { data, isLoading } = await useFetch(mealsUrl);
+
+  state.mealsData = data.value;
+  state.isLoadingMeals = isLoading.value;
+
+  console.log(data.value.hits);
+});
+
+async function fetchMoreMeals() {
+  const nextMealsUrl = state.mealsData._links.next.href;
+
+  if (state.mealsData.hits.length < 60) {
+    const { data } = await useFetch(nextMealsUrl);
+    state.mealsData.hits.push(...data.value.hits);
+  }
+  isFetching.value = false;
+}
+
 function extractIdFromUri(uri) {
   return uri.split('#recipe_').pop();
 }
@@ -39,33 +59,16 @@ function goToMealDetails(meal) {
     },
   });
 }
-
-onBeforeMount(async () => {
-  const { data, isLoading } = await useFetch(mealsUrl);
-
-  state.mealsData = data.value;
-  state.isLoadingMeals = isLoading.value;
-});
-
-async function fetchMoreMeals() {
-  const nextMealsUrl = state.mealsData._links.next.href;
-
-  if (state.mealsData.hits.length < 60) {
-    const { data } = await useFetch(nextMealsUrl);
-    state.mealsData.hits.push(...data.value.hits);
-  }
-  isFetching.value = false;
-}
 </script>
 
 <template>
-  <Transition name="slide-fade">
-    <Backdrop @closeModal="closeModal" v-if="isModalOpen" />
-  </Transition>
-
+  <div v-if="isModalOpen">
+    <Backdrop @closeModal="closeModal" />
+    <Modal />
+  </div>
   <div class="showcase">
     <img src="@/assets/food.jpg" alt="food" class="showcase-img" />
-    <SearchBox />
+    <SearchBox @openModal="openModal" />
   </div>
   <section class="section-top-meals container" ref="scrollComponent">
     <h2 class="heading-secondary">This weeks top recepies</h2>
@@ -86,5 +89,3 @@ async function fetchMoreMeals() {
 <style lang="scss" scoped>
 @use '@/sass/views/home';
 </style>
-
-<style lang="scss" scoped></style>
