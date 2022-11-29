5da19983f6e9a1a68500248d0fd0e544db036d4e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-29 12:23:12 +0100

    changed filter tag from p to button

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+const emit = defineEmits(['openModal']);
+
+function onOpenModal() {
+  emit('openModal');
+}
+</script>
 
 <template>
   <div class="search-box">
     <h1 class="search-box__heading">What would you like to cook?</h1>
     <div class="input-container">
-      <p class="filter-btn">Filter</p>
+      <button class="filter-btn" @click="onOpenModal">Filter</button>
       <font-awesome-icon icon="search" class="input-icon" />
       <input
         class="input-field"
