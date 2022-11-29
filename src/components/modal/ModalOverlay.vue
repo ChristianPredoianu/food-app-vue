948d13aff47b0bec72ea7955f08e7140f16806f8
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-29 12:23:41 +0100

    work on modalOverlay

--- src/components/modal/ModalOverlay.vue
+++ src/components/modal/ModalOverlay.vue
@@ -1,6 +1,58 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue';
 
-<template></template>
+import { meals } from '@/utils/filterData.js';
+
+import SelectDropdown from '@/components/filters/SelectDropdown.vue';
+
+const selectedOptions = reactive({
+  mealType: null,
+  dishType: null,
+  cuisineType: null,
+});
+
+/* function getMealType(val) {
+  selectedOptions.mealType = val;
+}
+
+function getDishType(val) {
+  selectedOptions.dishType = val;
+}
+
+function getCuisineType(val) {
+  selectedOptions.cuisineType = val;
+} */
+
+function test(val, index) {
+  val === '' ? (selectedOptions[index] = null) : (selectedOptions[index] = val);
+}
+</script>
+
+<template>
+  <div class="modal-overlay">
+    <div class="selects">
+      <div class="dropdown" v-for="(meal, index) in meals">
+        <SelectDropdown :meals="meal" :index="index" @emitValue="test" />
+      </div>
+
+      <!--    <SelectDropdown
+        :label="labels[1]"
+        :category="dishTypes"
+        @emitValue="getDishType"
+      />
+      <SelectDropdown
+        :label="labels[2]"
+        :category="cuisineTypes"
+        @emitValue="getCuisineType"
+      /> -->
+    </div>
+  </div>
+  <div class="meal-tags">
+    <div class="meal-tag" v-for="selectedOption in selectedOptions">
+      {{ selectedOption }}
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @use '@/sass/components/modal/modal-overlay';
