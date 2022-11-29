4962c1e1e3792640fff0702d170a3ce15e865840
Christian Predoianu <christianpredoianu@yahoo.com>
2022-11-29 12:23:26 +0100

    work on modal

--- src/components/modal/Modal.vue
+++ src/components/modal/Modal.vue
@@ -1,6 +1,14 @@
-<script setup></script>
+<script setup>
+import ModalOverlayVue from '@/components/modal/ModalOverlay.vue';
+</script>
 
-<template></template>
+<template>
+  <teleport to="#overlay-root">
+    <div class="modal">
+      <ModalOverlayVue />
+    </div>
+  </teleport>
+</template>
 
 <style lang="scss" scoped>
 @use '@/sass/components/modal/modal';
