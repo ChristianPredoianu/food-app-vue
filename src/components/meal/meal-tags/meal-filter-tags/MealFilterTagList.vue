ced43043aef48bb199bf8cf788f37fea1889ee54
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-01 14:39:46 +0100

    added filter tags

---
+++ src/components/meal/meal-tags/meal-filter-tags/MealFilterTagList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import MealFilterTagListItem from '@/components/meal/meal-tags/meal-filter-tags/MealFilterTagListItem.vue';
+
+const props = defineProps({
+  dishTags: Array,
+});
+
+const emit = defineEmits(['removeTag']);
+
+function onRemoveTag(tag) {
+  emit('removeTag', tag);
+}
+</script>
+
+<template>
+  <ul class="meal-tag-list">
+    <MealFilterTagListItem :dishTags="dishTags" @removeTag="onRemoveTag" />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/meal-tags/meal-filter-tags/meal-filter-tag-list';
+</style>
---
+++ src/components/meal/meal-tags/meal-filter-tags/MealFilterTagListItem.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useCapitalizeFirstLetter } from '@/composables/useCapitalizeFirstLetter';
+
+const props = defineProps({
+  dishTags: Array,
+});
+
+const emit = defineEmits(['removeTag']);
+
+function onRemoveTag(tag) {
+  emit('removeTag', tag);
+}
+
+const { capitalizeFirstLetter } = useCapitalizeFirstLetter();
+</script>
+
+<template>
+  <li
+    class="meal-tag-list-item"
+    v-for="tag in dishTags"
+    @click="onRemoveTag(tag)"
+  >
+    {{ capitalizeFirstLetter(tag) }}
+
+    <font-awesome-icon icon="xmark" class="meal-tag-list-item__remove" />
+  </li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/meal/meal-tags/meal-filter-tags/meal-filter-tag-list-item';
+</style>
