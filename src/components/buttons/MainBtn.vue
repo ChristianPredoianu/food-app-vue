58e702b21938cd088e37aa979b553edba8153457
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-12 15:36:39 +0100

    added MainBtn component

---
+++ src/components/buttons/MainBtn.vue
@@ -0,0 +1,15 @@
+<script setup>
+const emit = defineEmits(['handleClick']);
+
+function onHandleClick() {
+  emit('handleClick');
+}
+</script>
+
+<template>
+  <button class="main-btn" @click="onHandleClick"><slot></slot></button>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/buttons/main-btn';
+</style>
