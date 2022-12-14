5f664ec1b892884df7f264da2484d91d75e8f7a2
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-14 14:26:51 +0100

    fixed responsive issues + useFetch to fetch searchQuery

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,8 +1,18 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import { useQueryUrl } from '@/composables/useQueryUrl';
+import { useFetch } from '@/composables/useFetch';
 
 const isNavOpen = ref(false);
 const isMobileView = ref(true);
+const isSearchOpen = ref(false);
+const searchQuery = ref(null);
+
+const emit = defineEmits(['queryMeals']);
+
+function onQueryMeals(mealsData) {
+  emit('queryMeals', mealsData);
+}
 
 function toggleNav() {
   isNavOpen.value = !isNavOpen.value;
@@ -11,6 +21,23 @@ function toggleNav() {
 function handleView() {
   isMobileView.value = window.innerWidth < 640;
   isMobileView.value ? (isNavOpen.value = false) : (isNavOpen.value = true);
+}
+
+function toggleSearch() {
+  isSearchOpen.value = !isSearchOpen.value;
+}
+
+function closeSearch() {
+  isSearchOpen.value = false;
+  console.log('closing');
+}
+
+async function searchMeals() {
+  const { searchQueryUrl } = useQueryUrl(searchQuery.value);
+  const { data } = await useFetch(searchQueryUrl);
+
+  onQueryMeals(data.value);
+  closeSearch();
 }
 
 onMounted(() => {
@@ -28,14 +55,35 @@ onUnmounted(() => {
     <nav class="nav container">
       <p class="logo">Foo<span class="logo__span">die</span></p>
       <ul class="nav-links" v-if="isNavOpen">
-        <li class="nav-links__item">
-          <font-awesome-icon icon="search" />Search
+        <li class="nav-links__item" v-if="!isMobileView">
+          <div class="search-container">
+            <Transition name="slide-fade">
+              <input
+                type="text"
+                class="search-input"
+                placeholder="Search meals.."
+                @keyup.enter="searchMeals"
+                v-model="searchQuery"
+                v-if="isSearchOpen"
+            /></Transition>
+            <font-awesome-icon
+              icon="search"
+              class="search-icon"
+              @click="toggleSearch"
+            />
+          </div>
+          <p @click="toggleSearch">Search</p>
         </li>
-        <li class="nav-links__item">
+
+        <!--   <li class="nav-links__item" v-if="!isMobileView">
           <font-awesome-icon icon="fa-regular fa-heart" />Favorite
-        </li>
-        <li class="nav-links__item nav-links__item--btn-transparent">Login</li>
-        <li class="nav-links__item nav-links__item--btn-green">Sign up</li>
+        </li> -->
+        <div class="cta-btns">
+          <li class="nav-links__item nav-links__item--btn-transparent">
+            Login
+          </li>
+          <li class="nav-links__item nav-links__item--btn-green">Sign up</li>
+        </div>
       </ul>
       <div class="hamburger" :class="{ active: isNavOpen }" @click="toggleNav">
         <span class="hamburger__bar"></span>
