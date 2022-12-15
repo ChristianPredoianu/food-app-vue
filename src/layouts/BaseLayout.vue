d785beb11510c06e0f0c820c9c74b9e0517e59fa
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-15 13:56:18 +0100

    passed user search meals data to Home view

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,11 +1,30 @@
 <script setup>
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 import NavBar from '@/components/nav/NavBar.vue';
+
+const emit = defineEmits(['queryMeals']);
+
+const route = useRoute();
+
+const queriedMealsData = ref(null);
+
+// Pass the queriedMealsData only to Home view
+const propsToPass = computed(() => {
+  const mealsData = route.name === 'Home' ? queriedMealsData.value : undefined;
+  return mealsData;
+});
+
+function setMealsData(mealsData) {
+  queriedMealsData.value = mealsData;
+}
 </script>
 
 <template>
-  <NavBar />
-
-  <router-view />
+  <div>
+    <NavBar @queryMeals="setMealsData" />
+    <router-view :queriedMealData="propsToPass" />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
