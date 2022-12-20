56df981f7e176dbef9230117a53b7f66a97c4c60
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-20 15:09:16 +0100

    added AuthCard

---
+++ src/components/cards/AuthCard.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="auth-card">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/cards/auth-card';
+</style>
