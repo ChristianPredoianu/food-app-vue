da7c0cea50610133697699ea544545ba7be054c2
Christian Predoianu <christianpredoianu@yahoo.com>
2022-12-20 15:08:19 +0100

    added AuthSubmitBtn

---
+++ src/components/buttons/AuthSubmitBtn.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <button type="submit" class="submit-btn">Submit</button>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/components/buttons/auth-submit-btn';
+</style>
