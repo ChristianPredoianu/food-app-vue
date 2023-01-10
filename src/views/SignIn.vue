fecca3f8307d0291d02a58aee94195f3a35d9151
Christian Predoianu <christianpredoianu@yahoo.com>
2023-01-10 14:13:10 +0100

    added SignIn view

---
+++ src/views/SignIn.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SignInForm from '@/components/forms/SignInForm.vue';
+</script>
+
+<template>
+  <main class="main container">
+    <div class="sign-in-container">
+      <img
+        src="@/assets/food-plate.jpg"
+        alt="food"
+        class="sign-in-container__img"
+      />
+      <div class="form-container">
+        <SignInForm />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use '@/sass/views/sign-in';
+</style>
