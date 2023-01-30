ef7a977e58fb4983829889f0c5331feb0753bc41
Christian Predoianu <christianpredoianu@yahoo.com>
2023-01-30 15:14:53 +0100

    added firebase realtime database

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -4,6 +4,7 @@ import { getAnalytics } from 'firebase/analytics';
 const firebaseConfig = {
   apiKey: 'AIzaSyBRQgcZiyD9o4vVzOrGBWsEz8_mQ1e4ssk',
   authDomain: 'food-a-18243.firebaseapp.com',
+  databaseURL: 'https://food-a-18243-default-rtdb.firebaseio.com',
   projectId: 'food-a-18243',
   storageBucket: 'food-a-18243.appspot.com',
   messagingSenderId: '214825260236',
