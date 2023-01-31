73da2633381fe1ceefee52415ca6ffc677774690
Christian Predoianu <christianpredoianu@yahoo.com>
2023-01-31 14:30:17 +0100

    read data from firebase

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,7 +1,49 @@
-<script setup></script>
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import MealCard from '@/components/cards/MealCard.vue';
+import { getAuth } from 'firebase/auth';
+import { getDatabase, ref as dbRef, onValue } from 'firebase/database';
+
+const db = getDatabase();
+const auth = getAuth();
+
+const currentUser = ref(auth.currentUser);
+const userFavoriteMeals = ref(null);
+
+onBeforeMount(() => {
+  console.log('mounted');
+  const mealsListRef = dbRef(
+    db,
+    `users/ ${currentUser.value.uid}/favoriteMeals`
+  );
+
+  onValue(mealsListRef, (snapshot) => {
+    const data = snapshot.val();
+    userFavoriteMeals.value = data;
+    console.log(userFavoriteMeals.value);
+  });
+});
+</script>
 
 <template>
-  <h1>admin page</h1>
+  <main class="main container">
+    <h1 class="heading-primary">
+      {{
+        userFavoriteMeals !== null
+          ? 'Your favorite recepies:'
+          : 'You have no recepies as favorite'
+      }}
+    </h1>
+    <div class="meal-cards">
+      <MealCard
+        v-for="meal in userFavoriteMeals"
+        :key="meal.mealImage"
+        :meal="meal"
+      />
+    </div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '@/sass/views/admin';
+</style>
