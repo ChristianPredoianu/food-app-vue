fb70a82e13bcbb7b8331bd598d18f27cf3b940d1
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-07 13:12:07 +0100

    added some icons

--- src/main.js
+++ src/main.js
@@ -9,9 +9,22 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { faSearch, faFilter, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faInstagram,
+  faFacebook,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faSearch, faHeart, faFilter, faXmark);
+library.add(
+  faSearch,
+  faHeart,
+  faFilter,
+  faXmark,
+  faInstagram,
+  faFacebook,
+  faYoutube
+);
 
 let app;
 
