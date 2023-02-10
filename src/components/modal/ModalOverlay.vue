07d476e6f9a6003c5734b1f6ae7386692fa8abf7
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-10 18:34:34 +0100

    extracted url to fetch to composable

--- src/components/modal/ModalOverlay.vue
+++ src/components/modal/ModalOverlay.vue
@@ -2,6 +2,7 @@
 import { meals } from '@/utils/filterData.js';
 import { useFetch } from '@/composables/useFetch';
 import { useFilterRecipies } from '@/composables/useFilterRecipies';
+import { useUrlToFetch } from '@/composables/useUrlToFetch';
 
 import SelectDropdown from '@/components/filters/SelectDropdown.vue';
 import MealFilterTagList from '@/components/meal/meal-tags/meal-filter-tags/MealFilterTagList.vue';
@@ -18,17 +19,14 @@ const {
   isSelectedOptions,
 } = useFilterRecipies();
 
+const { fetchUrl } = useUrlToFetch(selectedOptions);
+
 function onCloseModal() {
   emit('closeModal');
 }
 
 function onFilteredData(mealsData) {
   emit('filteredData', mealsData);
-}
-
-// Transform whitespace to %20 to be able to fetch from api.
-function containsWhitespace(str) {
-  return str.replace(/\s/g, '%20');
 }
 
 function removeTagHandler(tag) {
@@ -39,31 +37,10 @@ function getDropdownValue(value, index) {
   getSelectValue(value, index);
 }
 
-function fetchUrl() {
-  const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public';
-  const appId = import.meta.env.VITE_APP_ID;
-  const apiKey = import.meta.env.VITE_API_KEY;
-  const diet =
-    selectedOptions.diet !== null ? `&diet=${selectedOptions.diet}` : '';
-  const health =
-    selectedOptions.health !== null ? `&health=${selectedOptions.health}` : '';
-  const cuisineType =
-    selectedOptions.cuisineType !== null
-      ? `&cuisineType=${containsWhitespace(selectedOptions.cuisineType)}`
-      : '';
-  const dishType =
-    selectedOptions.dishType !== null
-      ? `&dishType=${containsWhitespace(selectedOptions.dishType)}`
-      : '';
-  const mealsUrl = `${baseUrl}&app_id=${appId}&app_key=${apiKey}${diet}${health}${cuisineType}${dishType}
-`;
-  return mealsUrl;
-}
-
 async function fetchFilteredMeals() {
   isOptionsSelected.value = isSelectedOptions();
 
-  const url = fetchUrl();
+  const url = fetchUrl(selectedOptions);
 
   if (isOptionsSelected.value) {
     const { data } = await useFetch(url);
