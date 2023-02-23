d7fd49d8b24c1d7efa9e36f89672f001f5d7d598
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-23 15:01:50 +0100

    removed console log

--- src/composables/useDishTags.js
+++ src/composables/useDishTags.js
@@ -3,7 +3,9 @@ import { computed } from 'vue';
 export function useDishTags(selectedOptions) {
   const dishTags = computed(() => {
     const dishValues = Object.values(selectedOptions);
+
     const tags = [];
+
     for (let i = 0; i < dishValues.length; i++) {
       if (dishValues[i] !== null) {
         tags.push(dishValues[i]);
@@ -16,6 +18,7 @@ export function useDishTags(selectedOptions) {
     const foundTag = dishTags.value
       .filter((element) => element === tag)
       .toString();
+
     for (const key in selectedOptions) {
       if (selectedOptions[key] === foundTag) {
         selectedOptions[key] = null;
