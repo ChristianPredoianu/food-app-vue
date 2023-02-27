eb94cbae478bf497dcfd2a8efe5ef190149fef01
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-27 19:02:44 +0100

    added computed

--- src/composables/url/useUrlToFetch.js
+++ src/composables/url/useUrlToFetch.js
@@ -1,10 +1,12 @@
+import { computed } from 'vue';
+
 export function useUrlToFetch(selectedOptions) {
   // Transform whitespace to %20 to be able to fetch from api.
   function containsWhitespace(str) {
     return str.replace(/\s/g, '%20');
   }
 
-  function fetchUrl() {
+  const fetchUrl = computed(() => {
     const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public';
     const appId = import.meta.env.VITE_APP_ID;
     const apiKey = import.meta.env.VITE_API_KEY;
@@ -14,6 +16,10 @@ export function useUrlToFetch(selectedOptions) {
       selectedOptions.health !== null
         ? `&health=${selectedOptions.health}`
         : '';
+    const mealType =
+      selectedOptions.mealType !== null
+        ? `&mealType=${selectedOptions.mealType}`
+        : '';
     const cuisineType =
       selectedOptions.cuisineType !== null
         ? `&cuisineType=${containsWhitespace(selectedOptions.cuisineType)}`
@@ -22,11 +28,11 @@ export function useUrlToFetch(selectedOptions) {
       selectedOptions.dishType !== null
         ? `&dishType=${containsWhitespace(selectedOptions.dishType)}`
         : '';
-    const mealsUrl = `${baseUrl}&app_id=${appId}&app_key=${apiKey}${diet}${health}${cuisineType}${dishType}
+    const mealsUrl = `${baseUrl}&app_id=${appId}&app_key=${apiKey}${diet}${mealType}${health}${cuisineType}${dishType}
       `;
 
     return mealsUrl;
-  }
+  });
 
   return { fetchUrl };
 }
