762dda80ff5ba35f444ec8fe2f695f72d9e4414b
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-27 11:03:23 +0100

    added into folders

--- src/composables/useCapitalizeFirstLetter.js
+++
@@ -1,7 +0,0 @@
-export function useCapitalizeFirstLetter() {
-  function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-  return { capitalizeFirstLetter };
-}
--- src/composables/useExtractIdFromUri.js
+++
@@ -1,7 +0,0 @@
-export function useExtractIdFromUri() {
-  function extractIdFromUri(uri) {
-    return uri.split('#recipe_').pop();
-  }
-
-  return { extractIdFromUri };
-}
