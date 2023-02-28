b4d32f3f58929be6da9c93d31f57d6102ddf177d
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-28 12:51:14 +0100

    removed is filtering ref & isFiltering emit

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,43 +1,25 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import { useQueryUrl } from '@/composables/url/useQueryUrl';
 import { useFetch } from '@/composables/useFetch';
 
 const emit = defineEmits([
   'openModal',
-  'queryMeals',
-  'isSearching',
+  'queriedMealsData',
   'fetchInitialMeals',
 ]);
 
 const searchQuery = ref(null);
-const isSearching = ref(false);
 
-const { queryUrl } = useQueryUrl();
+const { queryUrl } = useQueryUrl(searchQuery.value);
 const { data, fetchData } = useFetch();
 
-function onOpenModal() {
-  emit('openModal');
-}
-
-function onQueryMeals(mealsData) {
-  emit('queryMeals', mealsData);
-}
-
-function onSearching() {
-  emit('isSearching', isSearching);
-}
-
 async function fetchSearchQuery() {
-  isSearching.value = true;
-
   if (searchQuery.value !== null) {
     const url = queryUrl(searchQuery.value);
     await fetchData(url);
 
-    isSearching.value = false;
-
-    onQueryMeals(data.value);
+    onQueriedMealsData(data.value);
   }
 
   if (searchQuery.value === '') {
@@ -45,16 +27,17 @@ async function fetchSearchQuery() {
   }
 }
 
+function onOpenModal() {
+  emit('openModal');
+}
+
 function onFetchInitialMeals() {
   emit('fetchInitialMeals');
 }
 
-watch(
-  () => isSearching.value,
-  () => {
-    onSearching();
-  }
-);
+function onQueriedMealsData(mealsData) {
+  emit('queriedMealsData', mealsData);
+}
 </script>
 
 <template>
