4fa5306743d727e564859a348043ef7d8ac0da9e
Christian Predoianu <christianpredoianu@yahoo.com>
2023-02-28 12:50:39 +0100

    work

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -3,26 +3,46 @@ import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from '@/components/nav/NavBar.vue';
 
-const emit = defineEmits(['queryMeals']);
+const emit = defineEmits(['queryMeals', 'isFiltering']);
 
 const route = useRoute();
 
 const queriedMealsData = ref(null);
+const isUserFiltering = ref(null);
 
 // Pass the queriedMealsData only to Home view
-const propsToPass = computed(() => {
+const queriedMealDataProp = computed(() => {
   const mealsData = route.name === 'Home' ? queriedMealsData.value : undefined;
   return mealsData;
+});
+
+const isFilteringProp = computed(() => {
+  const isFilteringMeals =
+    route.name === 'Home' ? isUserFiltering.value : undefined;
+  return isFilteringMeals;
 });
 
 function setMealsData(mealsData) {
   queriedMealsData.value = mealsData;
 }
+
+function setIsFiltering(isFiltering) {
+  isUserFiltering.value = isFiltering.value;
+}
 </script>
 
 <template>
-  <NavBar :key="route.fullPath" @queryMeals="setMealsData" />
-  <router-view :queriedMealData="propsToPass" />
+  <div>
+    <NavBar
+      :key="route.fullPath"
+      @queryMeals="setMealsData"
+      @isFiltering="setIsFiltering"
+    />
+    <router-view
+      :queriedMealData="queriedMealDataProp"
+      :isNavFiltering="isFilteringProp"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
